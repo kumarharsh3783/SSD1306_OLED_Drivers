f0511b7874d8dfea977adcac15e2c60602840b12
kumarharsh3783 <kumarharsh3783@gmail.com>
2024-07-07 20:59:42 +0530

    ssd1306 Driver Implementation

--- SSD1306_Drivers/inc/SSD1306.h
+++ SSD1306_Drivers/inc/SSD1306.h
@@ -8,5 +8,19 @@
 #ifndef SSD1306_H_
 #define SSD1306_H_
 
+/******************************
+ * Defines
+ *****************************/
+
+/* OLED Slave device Address - 0011 1100b */
+#define SSD1306_Addr	0x3C
+
+#define SSD1306_Read	1u
+#define SSD1306_Write	0u
+
+/* R/W#=1, it is in read mode. R/W#=0, it is in write mode */
+#define oled_read(addr)		((addr << 1) | SSD1306_Read)
+#define oled_write(addr)    ((addr << 1) | SSD1306_Write)
+
 
 #endif /* SSD1306_H_ */
